cc61c876e362fe8c94abb2cbf9a763f35a6d6999
gearDev-code <wslysdev@gmail.com>
2021-09-06 19:36:54 -0500

    Updated

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,25 @@
 const app = Vue.createApp({
         setup () {
-          return {}
+          return {
+            canvas: null,
+          }
+        },
+        mounted () {
+          this.canvas = new PIXI.Application({width: window.innerWidth, height:window.innerHeight-100})
+
+          document.getElementById("pixi-bg").append(this.canvas.view)
+
+          PIXI.loader
+          .add("./assets/building.png")
+          .load(this.init)
+        },
+        methods: {
+          loaded: function init() {
+            let building = new PIXI.Sprite(
+              PIXI.loader.resources["./assets/building.png"].texture
+            )
+            this.canvas.stage.addChild(building);
+          }
         }
       })
 
